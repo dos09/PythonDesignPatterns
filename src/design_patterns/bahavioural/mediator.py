115264316af783fdf877f5e3497b29349f33d55d
Zhulien Yankov <zhulien.yankov@sirma.bg>
2019-11-14 18:24:49 +0200

    add mediator pattern example

---
+++ src/design_patterns/bahavioural/mediator.py
@@ -0,0 +1,42 @@
+"""
+Objects communicate to each other using mediator, not directly
+"""
+
+
+class ChatRoom:
+
+    def __init__(self):
+        self.members = []
+
+    def add_member(self, member):
+        self.members.append(member)
+
+    def notify(self, sender, msg):
+        for member in self.members:
+            if member.name != sender.name:
+                member.display_msg('[%s]: %s' % (sender.name, msg))
+
+
+class Member:
+
+    def __init__(self, name, chat_room):
+        self.name = name
+        self.chat_room = chat_room  # mediator
+        self.chat_room.add_member(self)
+
+    def send_msg(self, msg):
+        self.chat_room.notify(self, msg)
+
+    def display_msg(self, msg):
+        print('chat window of %s: %s' % (self.name, msg))
+
+
+if __name__ == '__main__':
+    chat_room = ChatRoom()
+    asen = Member('Asen', chat_room)
+    kiro = Member('Kiro', chat_room)
+    ivan = Member('Ivan', chat_room)
+    asen.send_msg('opa')
+    asen.send_msg('az sym ot Sliven')
+    ivan.send_msg('dobre')
+
