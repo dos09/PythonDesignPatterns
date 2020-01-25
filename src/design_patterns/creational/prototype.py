a65b86d180e052252e40058cd321ca2c7ca2f35e
dos09 <fake@fake.com>
2020-01-25 13:47:10 +0200

    improve prototype design pattern example

--- src/design_patterns/creational/prototype.py
+++ src/design_patterns/creational/prototype.py
@@ -2,6 +2,16 @@
 When the initialization of object X is heavy/time-consuming,
 instead of initializing new instance every time, initialize it once,
 cache it and when instance is needed, just copy from the cache.
+
+Keeping objects in cache is not needed. Object can be copied and passed
+to method when the original object should not be changed
+Example:
+    obj = ClassX()
+    # need to have clone method in ClassX
+    do_sth_that_will_change_obj(obj.clone())
+    # or
+    # need to override __copy__ and/or __deepcopy__ in ClassX
+    do_sth_that_will_change_obj(copy.deepcopy(obj))
 """
 import copy
 
@@ -26,7 +36,7 @@ class Cache:
         }
 
     def get(self, humanoid_type):
-        return copy.deepcopy(self.type_humanoid_map.get(humanoid_type))
+        return copy.deepcopy(self.type_humanoid_map[humanoid_type])
 
     
 class Humanoid:
@@ -34,16 +44,19 @@ class Humanoid:
     def __init__(self):
         self.name = 'N/A'
         self.stat = {
-            'hp': 10,
+            'hp': 100,
             'kills': 0
         }
     
     def __copy__(self):
-        obj = self.__class__(self.name)
+#         print('copy')
+        obj = self.__class__()
+        obj.name = self.name
         obj.stat = copy.copy(self.stat)
         return obj
         
     def __deepcopy__(self, memo):
+#         print('deepcopy')
         obj = self.__class__()
         for attr_name, value in self.__dict__.items():
             setattr(obj, attr_name, copy.deepcopy(value, memo))
@@ -74,7 +87,7 @@ if __name__ == '__main__':
     orc1.show()
     orc2.show()
     orc1.name = 'orc1'
-    orc1.stat['hp'] = 11
+    orc1.stat['hp'] = 111
     orc2.name = 'orc2'
     orc1.show()
     orc2.show()
